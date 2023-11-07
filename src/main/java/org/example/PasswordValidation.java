e0bd8a93a9c9bd38584a1bdd622a376a92615020
LUEJ9 <juan.luethy@coop.ch>
2023-11-07 09:53:23 +0100

    Given Passwords Function and Test added, Special Character Funktion and Test added

--- src/main/java/org/example/PasswordValidation.java
+++ src/main/java/org/example/PasswordValidation.java
@@ -10,21 +10,20 @@ public class PasswordValidation {
         System.out.println(lenghIf);
         boolean numIf = numberTest(pw);
         System.out.println(numIf);
-        boolean  upperLowercaseTest = UpperLowercaseTest(pw);
+        boolean upperLowercaseTest = UpperLowercaseTest(pw);
         System.out.println(upperLowercaseTest);
     }
-    public static boolean lenghTest (String pw){
-        if (pw.length() >= 8){
-            return true;
-        }
-        return false;
+
+    public static boolean lenghTest(String pw) {
+        return pw.length() >= 8;
     }
-    public static boolean numberTest(String pw){
+
+    public static boolean numberTest(String pw) {
         boolean ifNum = pw.matches(".*\\d+.*");
         return ifNum;
     }
 
-    public static boolean UpperLowercaseTest (String pw) {
+    public static boolean UpperLowercaseTest(String pw) {
         boolean hasUpperCase = !pw.equals(pw.toLowerCase());
         boolean hasLowerCase = !pw.equals(pw.toUpperCase());
         if (hasUpperCase && hasLowerCase) {
@@ -34,5 +33,24 @@ public class PasswordValidation {
 
         }
 
+    }
+
+    public static boolean givenPwTest(String pw) {
+        String[] givenPws = new String[3];
+        givenPws[0] = "P@ssw0rd42";
+        givenPws[1] = "Password123";
+        givenPws[2] = "Pasword123";
+
+        for (int i = 0; i < givenPws.length; i++) {
+            if (pw.equals(givenPws[i])){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean specialCharTest(String pw){
+        return pw.matches(".*[^a-zA-Z0-9].*");
     }
 }
--- src/test/java/org/example/PasswordValidationTest.java
+++ src/test/java/org/example/PasswordValidationTest.java
@@ -1,8 +1,53 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class PasswordValidationTest {
+    @Test
+    void ifLengh_more_than_seven_return_true(){
+        String pw = "Passwort123";
+        boolean actual = PasswordValidation.lenghTest(pw);
+
+        assertEquals(true, actual);
+    }
+
+    @Test
+    void if_Contains_Nubers_return_True(){
+        String pw = "Passwort123";
+        boolean actual = PasswordValidation.numberTest(pw);
+
+        assertEquals(true, actual);
+    }
+
+    @Test
+    void If_Contains_Upper_and_Lowercase_return_True(){
+        String pw = "Passwort123";
+        boolean actual = PasswordValidation.UpperLowercaseTest(pw);
+
+        assertEquals(true, actual);
+    }
+
+    @Test
+    void If_One_of_given_Password_return_false(){
+        String pw = "Password123";
+        boolean actual = PasswordValidation.givenPwTest(pw);
+
+        assertEquals(false, actual);
+
+    }
+    @Test
+    void If_Contains_Special_Characters_return_true(){
+        String pw = "Password123";
+        boolean actual = PasswordValidation.specialCharTest(pw);
+
+        assertEquals(false, actual);
+
+    }
+
+
+
 
 }
